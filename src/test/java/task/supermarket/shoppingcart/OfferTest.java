64de326bb7a814d4f454774398afa7f2996f122a
samir.aghayarov <samir.aghayarov>
2023-01-16 17:26:59 +0400

    commit red 'buy_1_get_half_off offer'

--- src/test/java/task/supermarket/shoppingcart/OfferTest.java
+++ src/test/java/task/supermarket/shoppingcart/OfferTest.java
@@ -25,4 +25,22 @@ class OfferTest {
         Map<String, OfferStrategy> offers = shoppingCart.getOffers();
         Assertions.assertSame(OfferStrategy.BUY_2_GET_1_FREE, offers.get(itemName));
     }
+
+    @Test
+    void buyTwoGetOneOfferTest() {
+        //Arrange
+        String itemName = "book";
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Product book = new Product(itemName, BigDecimal.valueOf(10));
+
+        //Act
+        shoppingCart.addItem(book, 10);
+
+        Offer offer = new BuyOneGetHalfOffOffer();
+        offer.apply(shoppingCart, itemName);
+
+        //Assert
+        Map<String, OfferStrategy> offers = shoppingCart.getOffers();
+        Assertions.assertSame(OfferStrategy.BUY_1_GET_HALF_OFF, offers.get(itemName));
+    }
 }
